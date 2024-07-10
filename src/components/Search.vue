a06f06c28d8f9ab18b1379765ec89c46f0a43a42
oussama.bensalah <oussama.bensalah@amaris.com>
2024-07-10 18:53:59 +0200

    enhanced search by name results

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,6 +3,7 @@ import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 import { useProductStore } from "../store/product";
 import { baseUrl } from "../routes";
+import { toCurrency } from "../utils";
 
 const productStore = useProductStore();
 const router = useRouter();
@@ -27,23 +28,36 @@ const navigate = (id: number) => {
     <div class="form-control">
       <input
         v-model="input"
-        type="text"
+        type="search"
         placeholder="Search a product name..."
         class="input input-ghost"
         :disabled="!productStore.loaded"
       />
     </div>
     <ul
-      v-if="searchResults.length > 0"
-      class="shadow menu dropdown-content bg-base-100 rounded-box w-64 text-base-content overflow-y-scroll"
+      v-if="searchResults.length"
+      class="shadow menu dropdown-content bg-base-100 rounded-box w-96 text-base-content overflow-y-scroll"
       style="max-height: 50vh; z-index: 1"
     >
       <li v-for="product in searchResults" :key="product.id">
-        <a
-          href="#"
-          @click.prevent="navigate(product.id)"
-          v-text="product.title"
-        />
+        <a href="#" @click.prevent="navigate(product.id)">
+          <div class="flex flex-row">
+            <div class="flex-none w-14">
+              <img
+              :src="product.image"
+              alt="Product Image"
+              class="w-8 h-8 mr-5"
+            />
+            </div>
+            <div class="flex-1 w-64">
+              <h2 class="text-sm">{{ product.title }}</h2>
+            </div>
+            <div class="flex-1 w-32">
+              <p class="font-bold">{{ toCurrency(product.price) }}</p>
+            </div>
+           
+          </div>
+        </a>
       </li>
     </ul>
   </div>
